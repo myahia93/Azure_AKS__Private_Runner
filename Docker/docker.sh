a01bd26fe6602067cdf9db1e8cb9784d7bac87dc
YAHIA <mohcine.yahia@fujitsu.com>
2023-11-16 13:56:04 +0100

    first commit

--- Docker/docker.sh
+++ Docker/docker.sh
@@ -9,17 +9,17 @@ docker run -p 80:8080 -it --name ctn-name image-name
 # Enter to container :
 docker exec -it ctn-name bash
 # Tag :
-docker tag image-name acragentsdevops.azurecr.io/image-name:tag-name
+docker tag image-name TO_BE_DEFINED.azurecr.io/image-name:tag-name
 # Login to ACR :
-docker login acragentsdevops.azurecr.io -u USERNAME
+docker login TO_BE_DEFINED.azurecr.io -u USERNAME
 # Push To ACR :
-docker push acragentsdevops.azurecr.io/image-name:tag-name
+docker push TO_BE_DEFINED.azurecr.io/image-name:tag-name
 
 # Azure Pipelines :
 
 # Run the container build agent in host machine :
 docker run \
-    -e AZP_URL=https://dev.azure.com/amcs-apps \
+    -e AZP_URL=https://dev.azure.com/TO_BE_DEFINED \
     -e AZP_TOKEN= \
-    -e AZP_POOL=k8s-agent-pool \
+    -e AZP_POOL=TO_BE_DEFINED \
     self-hosted-agent:latest
